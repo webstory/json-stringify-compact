bfb165574cdc964a6d613517a4bacb497083edd1
Hoya Kim <hoya@mychar.info>
2026-03-10 12:11:42 +0900

    Added my specific use-case example * AKView annotation schema v5

--- examples/akview.ts
+++ examples/akview.ts
@@ -7,9 +7,9 @@ const input = await Bun.file(`${import.meta.dir}/input-akview-v5.json`).json();
 const output = stringifyCompact(input, {
   space: 2,
   rules: [
-    { path: '/annotations/timeRange', decimals: 1 },
-    { path: '/annotations/*/bbox[*]/t', decimals: 1 },
-    { path: '/annotations/*/bbox[*]/points', decimals: 4 },
+    { path: 'annotations[*].timeRange[*]', decimals: 1 },
+    { path: 'annotations[*].bbox[*].t', decimals: 1 },
+    { path: 'annotations[*].bbox[*].points[*]', decimals: 4 },
   ],
 });
 
